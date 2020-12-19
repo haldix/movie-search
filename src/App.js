0d4bfac87e5d0f31a13286361c42bc1c42f9a442
haldix <hjdpirate@yahoo.com>
2020-12-18 19:28:35 -0500

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <h1>Hello World</h1>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
